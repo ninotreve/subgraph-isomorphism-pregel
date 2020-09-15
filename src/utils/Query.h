a7c689b864004714da6ebbae39d99824dfe84ecd
Yuejia Zhang <nina_shanghai@hotmail.com>
2020-09-15 14:33:29 +0800

    add option -order

--- src/utils/Query.h
+++ src/utils/Query.h
@@ -6,7 +6,7 @@
 template <class NodeT>
 class Query {
 public:
-	virtual void init() = 0;
+	virtual void init(const string &order) = 0;
     virtual void addNode(char* line) = 0;
     virtual void printOrder() = 0;
 
